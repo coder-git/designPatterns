f7b25dc390fd1010ac26543771441ba5bcdd7a9b
zjl <super@man.com>
2022-01-27 22:20:15 +0800

    create new spring boot object for designPatterns

---
+++ mode1/src/main/java/com/design/mode1/Mode1Application.java
@@ -0,0 +1,13 @@
+package com.design.mode1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mode1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mode1Application.class, args);
+	}
+
+}
